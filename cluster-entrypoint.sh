d128bc4c752c6cb1efe9a1d568fdc3cb62135a81
Eric Burcham <jameseburcham@gmail.com>
2018-11-28 16:21:33 -0600

    Added high-availability policy creation for all exchanges and queues on startup.

--- cluster-entrypoint.sh
+++ cluster-entrypoint.sh
@@ -10,6 +10,9 @@ set -e
 # Do the cluster dance
 rabbitmqctl stop_app
 rabbitmqctl join_cluster rabbit@rabbitmq1
+rabbitmqctl start_app
+rabbitmqctl set_policy --priority 0 --apply-to "exchanges" ha-exchanges ".*" '{"ha-mode": "all"}'
+rabbitmqctl set_policy --priority 0 --apply-to "queues" ha-queues ".*" '{"ha-mode": "all"}'
 
 # Stop the entire RMQ server. This is done so that we
 # can attach to it again, but without the -detached flag
